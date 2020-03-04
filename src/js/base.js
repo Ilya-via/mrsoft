a287e6ba4ab6945fcc2829c3bc4940b69b7e71e2
Ilya-via <ilya95v@gmail.com>
2020-03-04 12:46:56 +0300

    gulp tasks

---
+++ src/js/base.js
@@ -0,0 +1,7 @@
+//PRELOADER
+// $(window).on('load', function () {
+//     $preloader = $('.loaderArea'),
+//         $loader = $preloader.find('.loader');
+//     $loader.fadeOut();
+//     $preloader.delay(300).fadeOut('slow');
+// });
